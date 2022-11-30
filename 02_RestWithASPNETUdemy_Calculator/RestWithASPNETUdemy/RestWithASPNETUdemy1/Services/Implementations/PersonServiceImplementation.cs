8c4e0d5e071af921d064508ea6c1d58ad459d19e
Jarleson oliveira <jarlesonmello@gmail.com>
2022-11-30 08:56:28 -0300

    Adicionando comentários no PersonServiceImplementation

--- 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy1/Services/Implementations/PersonServiceImplementation.cs
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy1/Services/Implementations/PersonServiceImplementation.cs
@@ -17,15 +17,22 @@ namespace RestWithASPNETUdemy.Services.Implementations
             _context = context;
         }
 
+        // Método responsável por retornar todas as pessoas
         public List<Person> FindAll() 
         {
             return _context.Persons.ToList();
         }
 
+        // Método responsável por retornar uma pessoa
         public Person FindByID(long id) 
         {
             return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
         }
+
+        // Metodo responsável por criar uma nova pessoa
+        // nesse momento adicionamos o objeto ao contexto
+        // e finalmente salvamos as mudanças no contexto
+        // na base de dados
         public Person Create(Person person) 
         {
             try 
@@ -40,10 +47,16 @@ namespace RestWithASPNETUdemy.Services.Implementations
             }
             return person;
         }
+
+        // Método responsável por atualizar uma pessoa
         public Person Update(Person person) 
         {
+            // Verificamos se a pessoa existe na base
+            // Se não existir retornamos uma instancia vazia de pessoa
             if (!Exists(person.Id)) return new Person();
 
+            // Pega o estado atual do registro no banco
+            // seta as alterações e salva
             var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
             if (result != null) 
             {
@@ -61,6 +74,9 @@ namespace RestWithASPNETUdemy.Services.Implementations
             }
             return person;
         }
+
+        // Método responsável por deletar
+        // uma pessoa a partir de um ID
         public void Delete(long id) 
         {
             var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
