a6e0f5a4c2f1e4b245c0a553d3dfd3263ceaeded
Jarleson oliveira <jarlesonmello@gmail.com>
2023-04-18 08:34:08 -0300

    Alterações antigas

--- 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy2/RestWithASPNETUdemy2/Business/IBookBusiness.cs
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy2/RestWithASPNETUdemy2/Business/IBookBusiness.cs
@@ -1,5 +1,6 @@
 ﻿using APIAspNetCore5.Data.VO;
 using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Hypermedia.Utils;
 using System.Collections.Generic;
 
 namespace APIAspNetCore5.Business
@@ -7,10 +8,11 @@ namespace APIAspNetCore5.Business
     public interface IBookBusiness
     {
         BookVO Create(BookVO book);
-        BookVO FindById(long id);
+        BookVO FindByID(long id);
         List<BookVO> FindAll();
+        PagedSearchVO<BookVO> FindWithPagedSearch(
+            string title, string sortDirection, int pageSize, int page);
         BookVO Update(BookVO book);
         void Delete(long id);
-        bool Exists(long id);
     }
 }
--- 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy2/RestWithASPNETUdemy2/Business/Implementations/BookBusinessImplementation.cs
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy2/RestWithASPNETUdemy2/Business/Implementations/BookBusinessImplementation.cs
@@ -3,6 +3,7 @@ using APIAspNetCore5.Data.VO;
 using APIAspNetCore5.Model;
 using APIAspNetCore5.Repository;
 using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Hypermedia.Utils;
 using System;
 using System.Collections.Generic;
 
@@ -19,38 +20,65 @@ namespace APIAspNetCore5.Business.Implementations
             _repository = repository;
             _converter = new BookConverter();
         }
-
-        public BookVO Create(BookVO book)
-        {
-            var BookEntity = _converter.Parse(book);
-            BookEntity = _repository.Create(BookEntity);
-            return _converter.Parse(BookEntity);
-        }
-
-        BookVO IBookBusiness.FindById(long id)
-        {
-            return _converter.Parse(_repository.FindById(id));
-        }
-
-        List<BookVO> IBookBusiness.FindAll()
+        // Method responsible for returning all people,
+        public List<BookVO> FindAll()
         {
             return _converter.Parse(_repository.FindAll());
         }
 
+        public PagedSearchVO<BookVO> FindWithPagedSearch(
+            string title, string sortDirection, int pageSize, int page)
+        {
+            var sort = (!string.IsNullOrWhiteSpace(sortDirection)) && !sortDirection.Equals("desc") ? "asc" : "desc";
+            var size = (pageSize < 1) ? 10 : pageSize;
+            var offset = page > 0 ? (page - 1) * size : 0;
+
+            string query = @"select * from books p where 1 = 1 ";
+            if (!string.IsNullOrWhiteSpace(title)) query = query + $" and p.title like '%{title}%' ";
+            query += $" order by p.title {sort} limit {size} offset {offset}";
+
+            string countQuery = @"select count(*) from books p where 1 = 1 ";
+            if (!string.IsNullOrWhiteSpace(title)) countQuery = countQuery + $" and p.title like '%{title}%' ";
+
+            var books = _repository.FindWithPagedSearch(query);
+            int totalResults = _repository.GetCount(countQuery);
+
+            return new PagedSearchVO<BookVO>
+            {
+                CurrentPage = page,
+                List = _converter.Parse(books),
+                PageSize = size,
+                SortDirections = sort,
+                TotalResults = totalResults
+            };
+        }
+
+        // Method responsible for returning one book by ID
+        public BookVO FindByID(long id)
+        {
+            return _converter.Parse(_repository.FindById(id));
+        }
+
+        // Method responsible to crete one new book
+        public BookVO Create(BookVO book)
+        {
+            var bookEntity = _converter.Parse(book);
+            bookEntity = _repository.Create(bookEntity);
+            return _converter.Parse(bookEntity);
+        }
+
+        // Method responsible for updating one book
         public BookVO Update(BookVO book)
         {
-            var BookEntity = _converter.Parse(book);
-            BookEntity = _repository.Update(BookEntity);
-            return _converter.Parse(BookEntity);
+            var bookEntity = _converter.Parse(book);
+            bookEntity = _repository.Update(bookEntity);
+            return _converter.Parse(bookEntity);
         }
+
+        // Method responsible for deleting a book from an ID
         public void Delete(long id)
         {
             _repository.Delete(id);
-        }
-
-        public bool Exists(long id)
-        {
-            return _repository.Exists(id);
         }
     }
 }
